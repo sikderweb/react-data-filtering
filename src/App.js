275fa391b62a831a2b5b5e1b670bb359b976b43b
sikderweb <sikderweb@gmail.com>
2022-02-04 00:32:02 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Restaurent from "./components/basic/Restaurent";
+// import UseState from "./components/Hooks/useState";
+// import UseEffect from "./components/Hooks/useEffect";
+// import UseReducer from "./components/Hooks/useReducer";
+
+const App = () => {
+  return <>
+  <Restaurent/>
+  
+  </>
+}
+export default App;
